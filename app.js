0f8eaa3515d1c648e00ef8005443f8d6dbd45ddc
DennisL10 <fl0368032019@unab.edu.sv>
2022-04-27 23:16:44 -0600

    cositas

---
+++ app.js
@@ -0,0 +1,40 @@
+//Invocamos express
+const express = require('express');
+const app = express();
+
+// seteamos urluncoded para capturar los datos del formulario
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Invocamos dotenv
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
+
+// seteamos el directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + 'public'));
+
+// Establecemos motor de plantillas
+app.set('view engine', 'ejs')
+
+// Invocamos bcryptjs
+const bcryptjs = require('bcryptjs');
+
+//var de session
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+
+
+console.log(__dirname);
+app.get('/', (req, res)=>{
+    res.send('BUENAS')
+})
+
+app.listen(3000, (req, res) =>{
+    console.log('SERVER RUNNING IN http://localhost:3000');
+})
