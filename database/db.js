59ecbd5d269733f99796d68329b21c89cd84d252
DennisL10 <fl0368032019@unab.edu.sv>
2022-04-29 00:52:50 -0600

    Arreglo login

--- database/db.js
+++ database/db.js
@@ -1,16 +1,19 @@
+// Invocamos a MySQL y realizamos la conexion
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+//Con variables de entorno
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE
 });
 
-connection.connect((error) =>{
-    if(error){
-        console.log('El error de conexion es: ' + error);
-        return;
+connection.connect((error)=>{
+    if (error) {
+      console.error('El error de conexión es: ' + error);
+      return;
     }
-    console.log('Conectado a la base de datos!');
-})
-module.exports = connection;
+    console.log('¡Conectado a la Base de Datos!');
+  });
+
+  module.exports = connection;
